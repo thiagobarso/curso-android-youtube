d0cdda211ce14a037fedac3adc14a27ce2847339
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 16:56:23 -0300

    aula 10

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActMain.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActMain.java
@@ -1,18 +1,29 @@
 package com.example.thiagosoares.carteiradeclientes;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.thiagosoares.carteiradeclientes.database.DadosOpenHelper;
+
 public class ActMain extends AppCompatActivity {
 
     private RecyclerView lstDados;
     private FloatingActionButton fab;
+    private ConstraintLayout layoutContentMain;
+
+    private SQLiteDatabase conexao;
+
+    private DadosOpenHelper dadosOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +32,29 @@ public class ActMain extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        lstDados = (RecyclerView) findViewById(R.id.lstDados);
-
         fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        lstDados = (RecyclerView) findViewById(R.id.lstDados);
+        layoutContentMain = (ConstraintLayout) findViewById(R.id.layoutContentMain);
+
+        criarConexao();
+    }
+
+    private void criarConexao(){
+        try{
+            dadosOpenHelper = new DadosOpenHelper(this);
+
+            conexao = dadosOpenHelper.getWritableDatabase();
+
+            Snackbar.make(layoutContentMain, R.string.message_conexao_criada_com_sucesso, Snackbar.LENGTH_SHORT)
+            .setAction(R.string.lbl_ok, null).show();
+
+        }catch(SQLException ex){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle(R.string.title_erro);
+            dlg.setMessage(ex.getMessage());
+            dlg.setNeutralButton("OK", null);
+            dlg.show();
+        }
     }
 
     public void cadastrar(View view){
--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/database/ScriptDLL.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/database/ScriptDLL.java
@@ -10,7 +10,7 @@ public class ScriptDLL {
         sql.append(" NOME VARCHAR(250) NOT NULL DEFAULT (''), ");
         sql.append(" ENDERECO VARCHAR(255) NOT NULL DEFAULT (''), ");
         sql.append(" EMAIL VARCHAR(200) NOT NULL DEFAULT (''), ");
-        sql.append(" TELEFONE VARCHAR(20) NOT NULL DEFAULT (''), ");
+        sql.append(" TELEFONE VARCHAR(20) NOT NULL DEFAULT ('') ");
         sql.append(") ");
 
         return sql.toString();
