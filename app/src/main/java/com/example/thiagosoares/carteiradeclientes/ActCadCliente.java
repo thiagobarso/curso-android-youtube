bb4480caac236530a42085d6c4111e9b9a632e1a
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 11:44:32 -0300

    aula 07 para aula 08

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
@@ -5,8 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActCadCliente extends AppCompatActivity {
 
@@ -29,4 +33,33 @@ public class ActCadCliente extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.menu_act_cad_cliente, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+
+            case R.id.action_ok:
+                Toast.makeText(this, "Botão Ok selecionado", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_cancelar:
+                Toast.makeText(this, "Botão cancelar selecionado", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
