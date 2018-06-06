66b123f2abeabb910dcc27fe6e5b1facbc8f50d5
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 17:18:57 -0300

    aula 12

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
@@ -1,21 +1,49 @@
 package com.example.thiagosoares.carteiradeclientes.dominio.repositorio;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
 import com.example.thiagosoares.carteiradeclientes.dominio.entidades.Cliente;
 
 import java.util.List;
 
 public class ClienteRepositorio {
 
-    public void inserir(Cliente cliente){
+    private SQLiteDatabase conexao;
 
+    public ClienteRepositorio(SQLiteDatabase conexao){
+        this.conexao = conexao;
+    }
+
+
+    public void inserir(Cliente cliente){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NOME", cliente.nome);
+        contentValues.put("ENDERECO", cliente.endereco);
+        contentValues.put("EMAIL", cliente.email);
+        contentValues.put("TELEFONE", cliente.telefone);
+
+        conexao.insertOrThrow("CLIENTE", null, contentValues);
     }
 
     public void excluir(int codigo){
+        String[] parametros = new String[1];
+        parametros[0] = String.valueOf(codigo);
 
+        conexao.delete("CLIENTE", "CODIGO = ?", parametros);
     }
 
     public void alterar(Cliente cliente){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NOME", cliente.nome);
+        contentValues.put("ENDERECO", cliente.endereco);
+        contentValues.put("EMAIL", cliente.email);
+        contentValues.put("TELEFONE", cliente.telefone);
 
+        String[] parametros = new String[1];
+        parametros[0] = String.valueOf(cliente.codigo);
+
+        conexao.update("CLIENTE", contentValues, "CODIGO = ?", parametros);
     }
 
     public List<Cliente> buscarTodos(){
