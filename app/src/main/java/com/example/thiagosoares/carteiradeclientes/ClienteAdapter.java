c6005bf642f1019df82aa113b657d3f4c5f780e5
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 18:19:14 -0300

    aula 16

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ClienteAdapter.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ClienteAdapter.java
@@ -2,23 +2,49 @@ package com.example.thiagosoares.carteiradeclientes;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class ClienteAdapter extends RecyclerView.Adapter {
+import com.example.thiagosoares.carteiradeclientes.dominio.entidades.Cliente;
+
+import java.util.List;
+
+public class ClienteAdapter extends RecyclerView.Adapter<ClienteAdapter.ViewHolderCliente> {
+
+    private List<Cliente> dados;
+
+    public ClienteAdapter(List<Cliente> dados) {
+        this.dados = dados;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolderCliente onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolderCliente holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dados.size();
+    }
+
+    public class ViewHolderCliente extends RecyclerView.ViewHolder{
+
+        public TextView txtNome;
+        public TextView txtTelefone;
+
+
+        public ViewHolderCliente(View itemView) {
+            super(itemView);
+
+            txtNome = (TextView) itemView.findViewById(R.id.txtNome);
+            txtTelefone = (TextView) itemView.findViewById(R.id.txtTelefone);
+        }
     }
 }
