1959fe2df33de2ffdf69c52fa6e0081b2046075c
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 12:10:38 -0300

    aula 08 para aula 09

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
@@ -1,10 +1,13 @@
 package com.example.thiagosoares.carteiradeclientes;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -33,6 +36,44 @@ public class ActCadCliente extends AppCompatActivity {
 
     }
 
+    private void validaCampos() {
+
+        boolean res = false;
+
+        String nome = edtNome.getText().toString();
+        String endereco = edtEndereco.getText().toString();
+        String email = edtEmail.getText().toString();
+        String telefone = edtTelefone.getText().toString();
+
+        if (res = isCampoVazio(nome)) {
+            edtNome.requestFocus();
+        } else if (res = isCampoVazio(endereco)) {
+            edtEndereco.requestFocus();
+        } else if (res = !isEmailValido(email)) {
+            edtEmail.requestFocus();
+        } else if (res = isCampoVazio(telefone)) {
+            edtTelefone.requestFocus();
+        }
+
+        if (res) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.title_aviso);
+            builder.setMessage(R.string.message_campos_invalidos_brancos);
+            builder.setNeutralButton(R.string.lbl_ok, null);
+            builder.show();
+        }
+
+    }
+
+    private boolean isCampoVazio(String valor) {
+
+        return (TextUtils.isEmpty(valor) || valor.trim().isEmpty());
+    }
+
+    private boolean isEmailValido(String email) {
+        return (!isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -51,12 +92,12 @@ public class ActCadCliente extends AppCompatActivity {
         switch (itemId) {
 
             case R.id.action_ok:
-                Toast.makeText(this, "Botão Ok selecionado", Toast.LENGTH_SHORT).show();
+                validaCampos();
+//                Toast.makeText(this, "Botão Ok selecionado", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.action_cancelar:
-                Toast.makeText(this, "Botão cancelar selecionado", Toast.LENGTH_SHORT).show();
-
+//                Toast.makeText(this, "Botão cancelar selecionado", Toast.LENGTH_SHORT).show();
                 break;
         }
 
