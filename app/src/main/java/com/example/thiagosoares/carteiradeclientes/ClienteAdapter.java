881bad8f0a32012b8a6a2671df7c8e2c7c4715a6
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 18:09:26 -0300

    aula 15

---
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ClienteAdapter.java
@@ -0,0 +1,24 @@
+package com.example.thiagosoares.carteiradeclientes;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ClienteAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
