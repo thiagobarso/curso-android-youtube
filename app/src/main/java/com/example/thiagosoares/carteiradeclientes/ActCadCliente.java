1b8eebbc8f74bf8f5ac8458b21c73ca379f23aed
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 11:08:23 -0300

    aula 05 para aula 06

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
@@ -9,7 +9,7 @@ import android.view.View;
 
 public class ActCadCliente extends AppCompatActivity {
 
-    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +18,6 @@ public class ActCadCliente extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
