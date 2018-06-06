3d43e92a914d714c29a0597e824dc7b79bbb5675
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 11:25:12 -0300

    aula 06 para aula 07

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActCadCliente.java
@@ -6,10 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class ActCadCliente extends AppCompatActivity {
 
-
+    private EditText edtNome;
+    private EditText edtEndereco;
+    private EditText edtEmail;
+    private EditText edtTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,11 @@ public class ActCadCliente extends AppCompatActivity {
         setContentView(R.layout.act_cad_cliente);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtEndereco = (EditText) findViewById(R.id.edtEndereco);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtTelefone = (EditText) findViewById(R.id.edtTelefone);
 
     }
 
