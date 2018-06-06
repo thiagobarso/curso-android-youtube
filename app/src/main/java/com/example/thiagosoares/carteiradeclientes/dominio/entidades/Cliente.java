9e21978332f51c81c2fdb0b2ff9a825b06f92820
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-06 17:06:37 -0300

    aula 11

---
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/dominio/entidades/Cliente.java
@@ -0,0 +1,11 @@
+package com.example.thiagosoares.carteiradeclientes.dominio.entidades;
+
+public class Cliente {
+
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public String email;
+    public String telefone;
+
+}
---
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
@@ -0,0 +1,28 @@
+package com.example.thiagosoares.carteiradeclientes.dominio.repositorio;
+
+import com.example.thiagosoares.carteiradeclientes.dominio.entidades.Cliente;
+
+import java.util.List;
+
+public class ClienteRepositorio {
+
+    public void inserir(Cliente cliente){
+
+    }
+
+    public void excluir(int codigo){
+
+    }
+
+    public void alterar(Cliente cliente){
+
+    }
+
+    public List<Cliente> buscarTodos(){
+        return null;
+    }
+
+    public Cliente buscarCliente(int codigo){
+        return null;
+    }
+}
