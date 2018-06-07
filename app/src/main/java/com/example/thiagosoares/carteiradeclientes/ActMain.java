c053dde068e939f21fb8aa35c8410220979c1cfb
Thiago Barbosa Soares <thiagosoares@MacBook-Pro-de-Thiago.local>
2018-06-07 15:50:22 -0300

    aula 18

--- app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActMain.java
+++ app/src/main/java/com/example/thiagosoares/carteiradeclientes/ActMain.java
@@ -9,11 +9,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.thiagosoares.carteiradeclientes.database.DadosOpenHelper;
+import com.example.thiagosoares.carteiradeclientes.dominio.entidades.Cliente;
+import com.example.thiagosoares.carteiradeclientes.dominio.repositorio.ClienteRepositorio;
+
+import java.util.List;
 
 public class ActMain extends AppCompatActivity {
 
@@ -22,6 +27,10 @@ public class ActMain extends AppCompatActivity {
     private ConstraintLayout layoutContentMain;
 
     private SQLiteDatabase conexao;
+
+    private ClienteRepositorio clienteRepositorio;
+
+    private ClienteAdapter clienteAdapter;
 
     private DadosOpenHelper dadosOpenHelper;
 
@@ -37,6 +46,19 @@ public class ActMain extends AppCompatActivity {
         layoutContentMain = (ConstraintLayout) findViewById(R.id.layoutContentMain);
 
         criarConexao();
+
+        lstDados.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        lstDados.setLayoutManager(linearLayoutManager);
+
+        clienteRepositorio = new ClienteRepositorio(conexao);
+
+        List<Cliente> dados = clienteRepositorio.buscarTodos();
+
+        clienteAdapter = new ClienteAdapter(dados);
+
+        lstDados.setAdapter(clienteAdapter);
     }
 
     private void criarConexao(){
